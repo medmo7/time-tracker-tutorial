2dec3f01e344dd59964e9e2ec2c68030fffe80c9
mohamed <bourga.mohamed@gmail.com>
2023-11-06 15:48:31 +0100

    Initial Commit

---
+++ time-tracker-tutorial/time_tracker_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  time_tracker_tutorialApp.swift
+//  time-tracker-tutorial
+//
+//  Created by BOURGA Mohamed on 6/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct time_tracker_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
